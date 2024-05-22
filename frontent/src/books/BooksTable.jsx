ad0656184bf119733de3d32dcfef72804145c79c
Subin <109493447+Subin43@users.noreply.github.com>
2024-05-22 17:23:37 +0530

    Update BooksTable.jsx

--- frontent/src/books/BooksTable.jsx
+++ frontent/src/books/BooksTable.jsx
@@ -3,37 +3,29 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 
-export default function BooksTable({ books }) {
+export default function BooksTable({ books = [] }) {
+  if (books.length === 0) {
+    return <p>No books available.</p>;
+  }
+
   return (
     <table className="w-full border-separate border-spacing-2">
       <thead>
         <tr>
           <th className="border border-slate-600 rounded-md">No</th>
           <th className="border border-slate-600 rounded-md">Title</th>
-          <th className="border border-slate-600 rounded-md max-md:hidden">
-            Author
-          </th>
-          <th className="border border-slate-600 rounded-md max-md:hidden">
-            Publish Year
-          </th>
+          <th className="border border-slate-600 rounded-md max-md:hidden">Author</th>
+          <th className="border border-slate-600 rounded-md max-md:hidden">Publish Year</th>
           <th className="border border-slate-600 rounded-md">Operations</th>
         </tr>
       </thead>
       <tbody>
         {books.map((book, index) => (
           <tr key={book._id} className="h-8">
-            <td className="border border-slate-700 rounded-md text-center">
-              {index + 1}
-            </td>
-            <td className="border border-slate-700 rounded-md text-center">
-              {book.title}
-            </td>
-            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-              {book.author}
-            </td>
-            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-              {book.publishedYear}
-            </td>
+            <td className="border border-slate-700 rounded-md text-center">{index + 1}</td>
+            <td className="border border-slate-700 rounded-md text-center">{book.title}</td>
+            <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.author}</td>
+            <td className="border border-slate-700 rounded-md text-center max-md:hidden">{book.publishedYear}</td>
             <td className="border border-slate-700 rounded-md text-center">
               <div className="flex justify-center gap-x-4">
                 <Link to={`/books/details/${book._id}`}>
