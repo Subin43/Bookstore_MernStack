96ae6669c3310218f4ec082069fb3add2059f409
Subin <109493447+Subin43@users.noreply.github.com>
2024-05-23 22:55:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,35 +1,38 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
 import mongoose from 'mongoose';
 import bookRoute from './routes/bookRoute.js';
 
 dotenv.config();
 
 const app = express();
-
-const PORT = process.env.PORT || 8000; // Set a default port if PORT is not provided in .env
+const PORT = process.env.PORT || 8000;
 const MONGO_URL = process.env.MONGO_URL;
+
+if (!MONGO_URL) {
+  console.error('MONGO_URL not defined in environment variables');
+  process.exit(1); // Exit the process with an error code
+}
+
 app.use(express.json());
-// control the unauthorized request from browser
-// Method: 1
-//app.use(cors())
-// method:2 
 app.use(cors({
-    origin: 'bookstore-mern-stack-frontend.vercel.app',
-    content:['GET','POST','DELETE',"PUT"],
-    credentials:true
-}))
-app.use('/books',bookRoute)
+  origin: 'bookstore-mern-stack-frontend.vercel.app',
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  credentials: true
+}));
+
+app.use('/books', bookRoute);
 
 mongoose
   .connect(MONGO_URL)
   .then(() => {
     console.log('App connected to database');
     app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
+      console.log(`App is listening on port: ${PORT}`);
     });
   })
   .catch((error) => {
-    console.log(error);
+    console.error('Failed to connect to the database:', error);
+    process.exit(1); // Exit the process with an error code
   });
