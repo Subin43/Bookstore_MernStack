0ba61fed62166cddcef3cb3dd871f0ba6f55297d
Subin <109493447+Subin43@users.noreply.github.com>
2024-05-24 09:53:24 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,34 +6,35 @@ import bookRoute from './routes/bookRoute.js';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGO_URL;
+const createServer = () => {
+  const app = express();
+  const PORT = process.env.PORT || 8000;
+  const MONGO_URL = process.env.MONGO_URL;
 
-if (!MONGO_URL) {
-  console.error('MONGO_URL not defined in environment variables');
-  process.exit(1); // Exit the process with an error code
-}
-
-app.use(express.json());
-app.use(cors({
-  origin: 'bookstore-mern-stack-frontend.vercel.app',
-  methods: ['GET', 'POST', 'DELETE', 'PUT'],
-  credentials: true
-}));
-app.use('/books', bookRoute);
-
-mongoose
-  .connect(MONGO_URL)
-  .then(() => {
-    console.log('App connected to database');
-  })
-  .catch((error) => {
-    console.error('Failed to connect to the database:', error);
+  if (!MONGO_URL) {
+    console.error('MONGO_URL not defined in environment variables');
     process.exit(1); // Exit the process with an error code
-  });
+  }
 
-// Export a default function that returns the Express app
-export default function () {
+  app.use(express.json());
+  app.use(cors({
+    origin: 'bookstore-mern-stack-frontend.vercel.app',
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    credentials: true
+  }));
+  app.use('/books', bookRoute);
+
+  mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+      console.log('App connected to database');
+    })
+    .catch((error) => {
+      console.error('Failed to connect to the database:', error);
+      process.exit(1); // Exit the process with an error code
+    });
+
   return app;
-}
+};
+
+export default createServer;
